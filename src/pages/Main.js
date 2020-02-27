258e29151966a097938e0344eedce0c542e3e283
moisesabraaoso <moisesdirt_bmx@hotmail.com>
2020-02-27 01:10:37 -0300

    iniciando basico do projeto para estudos

---
+++ src/pages/Main.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import CardView from 'react-native-rn-cardview';
+
+
+function Main() {
+    const superHero = [
+        {
+            name: 'Spider Man',
+            avatar: 'https://ingresso-a.akamaihd.net/img/cinema/cartaz/19184-cartaz.jpg'
+        },
+        {
+            name: 'America Captain',
+            avatar: 'https://amazonasatual.com.br/wp-content/uploads/2018/03/capit%C3%A3o-am%C3%A9rica.jpg'
+        },
+        {
+            name: 'Hulk',
+            avatar: 'https://images.tcdn.com.br/img/img_prod/723087/quadro_metal_hulk_avengers_5891_1_20191108184714.jpg'
+        },
+        {
+            name: 'Thor',
+            avatar: 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2019/11/legiao_7Rn5yMXUptNzr1oQGw9b_gkeuvmaAH063FjiKIEcDC.png.jpeg'
+        }
+    ];
+  return (
+    <CardView cardElevation={4}
+            maxCardElevation={4}
+            radius={10}
+            backgroundColor={'#ffffff'}>
+        <ScrollView>
+            {
+                superHero.map((hero) => (
+                    <View style={{padding:10}}>
+                        <View>
+                            <Image
+                                style={{width: 400, height: 600}}
+                                source={{uri:hero.avatar}}
+                            />
+                        </View>
+                        <View>
+                            <Text style={styles.textFont}>{hero.name}</Text>
+                        </View>
+                    </View>
+                ))
+            }
+        </ScrollView>
+    </CardView> 
+  );
+};
+
+const styles = StyleSheet.create({
+    textFont: {
+      color: '#7D40E7',
+      fontWeight: 'bold',
+      fontSize: 40,
+    }
+  });
+
+export default Main;
